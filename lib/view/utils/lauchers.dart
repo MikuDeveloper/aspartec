ef768fa835668338499fca94adaab663b08e0722
Jose Miguel Paniagua Tinajero <ttn.developer@gmail.com>
2024-05-26 18:23:00 -0600

    patching: Whatsapp launcher button.

--- lib/view/utils/lauchers.dart
+++ lib/view/utils/lauchers.dart
@@ -1,12 +1,13 @@
+import 'dart:io';
+
 import 'package:url_launcher/url_launcher.dart';
 
-import '../../firebase_options.dart';
 import 'show_alerts.dart';
 
 class Launchers {
   static Future<bool> openWhatsApp({required context, required String phoneNumber}) async {
     const message = 'Hola, te he solicitado una asesoría a través de la app ASPARTEC. ¡Vamos a trabajar!';
-    final url = DefaultFirebaseOptions.currentPlatform == DefaultFirebaseOptions.android
+    final url = Platform.isAndroid
         ? 'whatsapp://send?phone=$phoneNumber&text=${Uri.parse(message)}'
         : 'whatsapp://wa.me/$phoneNumber?text=${Uri.parse(message)}';
 
