1f4c0e6d003a1bd5152a1c6f049f6062183f5606
Jose Miguel Paniagua Tinajero <ttn.developer@gmail.com>
2024-08-11 00:33:46 -0600

    -changes: Report generation was cleaned. -features: Splash screen was added.

--- lib/view/home/advisor/reports_page.dart
+++ lib/view/home/advisor/reports_page.dart
@@ -110,105 +110,77 @@ class _ReportsPageState extends State<ReportsPage> {
     final PdfDocument document = PdfDocument(inputBytes: list);
 
     PdfForm form = document.form;
+    for (int i = 0; i < form.fields.count; i++) {
+      (form.fields[i] as PdfTextBoxField).font = PdfStandardFont(PdfFontFamily.helvetica, 9);
+    }
 
     // Date field
     PdfTextBoxField dateField = form.fields[0] as PdfTextBoxField;
     final date = DateTime.now();
     final dateText = '${date.day.toString().padLeft(2, '0')}-${date.month.toString().padLeft(2, '0')}-${date.year}';
-    dateField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     dateField.text = dateText;
-    dateField.readOnly = true;
 
     // Name field
     PdfTextBoxField nameField = form.fields[1] as PdfTextBoxField;
-    nameField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     nameField.text = '${userData.firstname} ${userData.lastname1} ${userData.lastname2 ?? ''}'.trim();
-    nameField.readOnly = true;
 
     // Major field
     PdfTextBoxField majorField = form.fields[2] as PdfTextBoxField;
-    majorField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     majorField.text = '${userData.major}';
-    majorField.readOnly = true;
 
     // Control number field
     PdfTextBoxField controlField = form.fields[3] as PdfTextBoxField;
-    controlField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     controlField.text = '${userData.controlNumber}';
-    controlField.readOnly = true;
 
     // Subjects
     PdfTextBoxField subjectsField = form.fields[4] as PdfTextBoxField;
-    subjectsField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     subjectsField.text = subjects;
-    subjectsField.readOnly = true;
 
     // Rating field
     PdfTextBoxField ratingField = form.fields[5] as PdfTextBoxField;
-    ratingField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     ratingField.text = rating.toStringAsFixed(1);
-    ratingField.readOnly = true;
 
     // Advices number field
     PdfTextBoxField advicesNumberField = form.fields[6] as PdfTextBoxField;
-    advicesNumberField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     advicesNumberField.text = '$advicesNumber';
-    advicesNumberField.readOnly = true;
 
     // Completed advices number field
     PdfTextBoxField canceledAdvicesField = form.fields[7] as PdfTextBoxField;
-    canceledAdvicesField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     canceledAdvicesField.text = '$canceledAdvicesNumber';
-    canceledAdvicesField.readOnly = true;
 
     // Completed advices number field
     PdfTextBoxField completedAdvicesField = form.fields[8] as PdfTextBoxField;
-    completedAdvicesField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     completedAdvicesField.text = '$completedAdvicesNumber';
-    completedAdvicesField.readOnly = true;
 
     // Sistemas field
     PdfTextBoxField sistemasField = form.fields[9] as PdfTextBoxField;
-    sistemasField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     sistemasField.text = '$sistemas';
-    sistemasField.readOnly = true;
 
     // Tics field
     PdfTextBoxField ticsField = form.fields[10] as PdfTextBoxField;
-    ticsField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     ticsField.text = '$tics';
-    ticsField.readOnly = true;
 
     // Meca field
     PdfTextBoxField mecaField = form.fields[11] as PdfTextBoxField;
-    mecaField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     mecaField.text = '$meca';
-    mecaField.readOnly = true;
 
     // Industrial field
     PdfTextBoxField industrialField = form.fields[12] as PdfTextBoxField;
-    industrialField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     industrialField.text = '$meca';
-    industrialField.readOnly = true;
 
     // Bioquimica field
     PdfTextBoxField bioquimicaField = form.fields[13] as PdfTextBoxField;
-    bioquimicaField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     bioquimicaField.text = '$bioquimica';
-    bioquimicaField.readOnly = true;
 
     // Gestion field
     PdfTextBoxField gestionField = form.fields[14] as PdfTextBoxField;
-    gestionField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     gestionField.text = '$gestion';
-    gestionField.readOnly = true;
 
     // Nano field
     PdfTextBoxField nanoField = form.fields[15] as PdfTextBoxField;
-    nanoField.font = PdfStandardFont(PdfFontFamily.helvetica, 9);
     nanoField.text = '$nano';
-    nanoField.readOnly = true;
 
+    form.flattenAllFields();
 
     File('$directoryPath/reporte-de-asesorias_${userData.controlNumber!}_$dateText.pdf').writeAsBytesSync(await document.save());
     document.dispose();
