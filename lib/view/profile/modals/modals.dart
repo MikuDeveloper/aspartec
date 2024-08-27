908bd4771beb0b993d055307c4ea1d0944b1c1e4
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2024-08-27 14:46:35 -0600

    Delete Account View

---
+++ lib/view/profile/modals/modals.dart
@@ -0,0 +1,4 @@
+export "package:aspartec/view/profile/modals/delete_account_bottom_sheet.dart";
+export "package:aspartec/view/profile/modals/update_academic_data_bottom_sheet.dart";
+export "package:aspartec/view/profile/modals/update_password_bottom_sheet.dart";
+export "package:aspartec/view/profile/modals/update_personal_data_bottom_sheet.dart";
